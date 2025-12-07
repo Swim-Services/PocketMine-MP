879ebd100196882c0b99950c57d69c39e4dea57e
GameParrot <85067619+GameParrot@users.noreply.github.com>
2025-12-06 20:25:43 -0500

    bare minumum .130

--- src/network/mcpe/convert/BlockTranslator.php
+++ src/network/mcpe/convert/BlockTranslator.php
@@ -48,6 +48,10 @@ final class BlockTranslator{
 			self::CANONICAL_BLOCK_STATES_PATH => '',
 			self::BLOCK_STATE_META_MAP_PATH => '',
 		],
+		ProtocolInfo::PROTOCOL_1_21_130 => [
+			self::CANONICAL_BLOCK_STATES_PATH => '',
+			self::BLOCK_STATE_META_MAP_PATH => '',
+		],
 		ProtocolInfo::PROTOCOL_1_21_120 => [
 			self::CANONICAL_BLOCK_STATES_PATH => '',
 			self::BLOCK_STATE_META_MAP_PATH => '',
@@ -205,6 +209,9 @@ final class BlockTranslator{
 	private static function setupHashProtocols() {
 		if (!isset(self::$HASH_PROTOCOLS)) {
 			self::$HASH_PROTOCOLS = [
+				ProtocolInfo::PROTOCOL_1_21_130 => function(BlockStateData $d) : BlockStateData {
+					return $d;
+				}
 			];
 		}
 	}
--- src/network/mcpe/convert/ItemTypeDictionaryFromDataHelper.php
+++ src/network/mcpe/convert/ItemTypeDictionaryFromDataHelper.php
@@ -46,6 +46,7 @@ final class ItemTypeDictionaryFromDataHelper{
 
 	private const PATHS = [
 		ProtocolInfo::CURRENT_PROTOCOL => "",
+		ProtocolInfo::PROTOCOL_1_21_130 => "",
 		ProtocolInfo::PROTOCOL_1_21_120 => "",
 		ProtocolInfo::PROTOCOL_1_21_111 => "-1.21.111",
 		ProtocolInfo::PROTOCOL_1_21_100 => "-1.21.100",
@@ -70,7 +71,7 @@ final class ItemTypeDictionaryFromDataHelper{
 		ProtocolInfo::PROTOCOL_1_20_10 => "-1.20.10",
 
 		ProtocolInfo::PROTOCOL_1_20_0 => "-1.20.0",
-/*
+		/*
 		ProtocolInfo::PROTOCOL_1_19_80 => "-1.19.80",
 
 		ProtocolInfo::PROTOCOL_1_19_70 => "-1.19.70",
